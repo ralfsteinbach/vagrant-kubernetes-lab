11c8571b102d23fa5d43ba538298a3d32cf740c4
Thomas Darimont <thomas.darimont@gmail.com>
2018-07-31 22:34:40 +0200

    Fix open commands in open-scripts on linux

--- scripts/open-grafana.sh
+++ scripts/open-grafana.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/set-kubeconfig.sh
 
-open http://127.0.0.1:3000
+xdg-open http://127.0.0.1:3000
 
 kubectl port-forward -n kube-system "$(kubectl get -n kube-system pod --selector=k8s-app=grafana -o jsonpath='{.items..metadata.name}')" 3000
--- scripts/open-weavescope.sh
+++ scripts/open-weavescope.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/set-kubeconfig.sh
 
-open http://127.0.0.1:4040
+xdg-open http://127.0.0.1:4040
 
 kubectl port-forward -n kube-system "$(kubectl get -n kube-system pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040
